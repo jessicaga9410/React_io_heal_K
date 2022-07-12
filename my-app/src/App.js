51c487467450c93c9caecce00e1916a16087e605
JESSICA GALARZA <jessicaga941@gmail.com>
2022-07-11 22:32:03 -0400

    added Header and Navbar files and folders and updated App.js with new files and imports

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from "./Components/Header/Header";
+import Navbar from "./Components/Navbar/Navbar";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <Navbar />
+   <Header />
+    </div>
+  );
+}
+
+export default App;
