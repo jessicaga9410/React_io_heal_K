5bf346f9a5100b3f9a27f4fc1069a8cc47adbbf6
JESSICA GALARZA <jessicaga941@gmail.com>
2022-07-11 22:46:00 -0400

    updated app.js and index.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import Header from "./Components/Header/Header";
-import Navbar from "./Components/Navbar/Navbar";
+import Header from "./Containers/Header/Header";
+import {Navbar} from "./Components";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+    <div className="gradient__bg">
     <Navbar />
    <Header />
+    </div>
     </div>
   );
 }
---
+++ my-app/src/Containers/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// import image1 from '../../assets/p3.jpg';
+import './header.css';
+
+const Header = () => (
+  <div className="header section__padding" id="home">
+    <div className="header-content">
+      <h1 className="gradient__text">Healing with Kate</h1>
+      <div className="header-content__input">
+
+        <button type="button">Book with me Here</button>
+      </div>
+
+    </div>
+  </div>
+);
+
+export default Header;
